f66845ee55a4a12200487f5942ba6951ab157102
fbwhdtn45 <fbwhdtn5882@naver.com>
2021-07-29 15:49:17 +0900

    4th_commit_0729

---
+++ minecraft_forget.py
@@ -0,0 +1,197 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import Canvas, N, E, W, S
+from tkinter import messagebox
+from tkinter.constants import ANCHOR, CENTER
+from PIL import ImageTk, Image
+import time
+
+class Forget(tk.Toplevel) :
+    def __init__(self):
+        super().__init__()
+        # tkinter 창 가로/세로 설정
+        self.width = int(self.winfo_screenwidth() / 5)
+        self.height = int(self.winfo_screenheight() / 4)
+
+        # 배경 투명 설정
+        self.wm_attributes('-transparentcolor',self['bg'])
+        
+        # 로그인 창 이미지 설정
+        self.image = ImageTk.PhotoImage(Image.open('assets/forget.jpg').resize((self.width,self.height), Image.ANTIALIAS))
+        
+        # 해상도 및 위치 설정
+        self.geometry('{}x{}+{}+{}'.format(self.width, self.height, int((self.winfo_screenwidth()-self.width)/2),int((self.winfo_screenheight()-self.height)/2)))
+        
+        # tk 설정
+        self.resizable(0,0)
+        self.title("아이디 / 비밀번호 찾기")
+
+        # 배경 설정
+        self.bg_label = tk.Label(self,image=self.image) 
+        self.bg_label.place(x=0,y=0, relwidth = 1, relheight = 1)
+
+        # -----------아이디 찾기------------
+        self.id_search_label = tk.Label(self,text='아이디', bg='snow2')
+        self.id_search_label.grid(row = 1, column = 1, columnspan = 6, sticky = 'ew')
+        # 이름
+        self.id_name_label = tk.Label(self,text="이름", bg='snow2')
+        self.id_name_label.grid(row = 2, column = 1)
+        # 이름 TEXT
+        self.id_name_textfield = tk.Entry(self, bg = 'linen')
+        self.id_name_textfield.grid(row = 2, column = 2,columnspan=5)
+
+        # 연락처
+        self.id_phone_label = tk.Label(self,text="연락처", bg='snow2')
+        self.id_phone_label.grid(row = 3, column = 1)
+        # 연락처 TEXT1
+        self.id_phone1_textfield = tk.Entry(self, bg = 'linen', width = 4)
+        self.id_phone1_textfield.grid(row = 3, column = 2, sticky= 'e')
+        # -
+        self.id_phone1_label = tk.Label(self,text="-", bg='snow2')
+        self.id_phone1_label.grid(row = 3, column = 3)
+        # 연락처 TEXT2
+        self.id_phone2_textfield = tk.Entry(self, bg = 'linen', width = 5)
+        self.id_phone2_textfield.grid(row = 3, column = 4,sticky = 'ew')
+        # -
+        self.id_phone2_label = tk.Label(self,text="-", bg='snow2')
+        self.id_phone2_label.grid(row = 3, column = 5)
+        # 연락처 TEXT3
+        self.id_phone3_textfield = tk.Entry(self, bg = 'linen', width = 6)
+        self.id_phone3_textfield.grid(row = 3, column = 6, sticky= 'w')
+        # 엔터키 이벤트 설정
+        self.id_phone3_textfield.bind('<Return>', self.id_enter)
+
+        # 찾기 버튼
+        self.register_btn = tk.Button(self,text="찾기", command = self.id_search, borderwidth=0, bg='salmon2',activebackground="salmon3")
+        self.register_btn.grid(row=4,column=2, sticky = 'ew', columnspan=4)
+
+        # --------비밀번호 찾기------------
+        self.pw_search_label = tk.Label(self,text='비밀번호', bg='snow2')
+        self.pw_search_label.grid(row = 5, column = 1, columnspan = 6, sticky = 'ew')
+        # ID
+        self.pw_id_label = tk.Label(self,text="ID", bg='snow2')
+        self.pw_id_label.grid(row = 6, column = 1)
+        # ID TEXT
+        self.pw_id_textfield = tk.Entry(self, bg = 'linen')
+        self.pw_id_textfield.grid(row = 6, column = 2,columnspan=5)
+
+        # 이름
+        self.pw_name_label = tk.Label(self,text="이름", bg='snow2')
+        self.pw_name_label.grid(row = 7, column = 1)
+        # 이름 TEXT
+        self.pw_name_textfield = tk.Entry(self, bg = 'linen')
+        self.pw_name_textfield.grid(row = 7, column = 2,columnspan=5)
+
+        # 연락처
+        self.pw_phone_label = tk.Label(self,text="연락처", bg='snow2')
+        self.pw_phone_label.grid(row = 8, column = 1)
+        # 연락처 TEXT1
+        self.pw_phone1_textfield = tk.Entry(self, bg = 'linen', width = 4)
+        self.pw_phone1_textfield.grid(row = 8, column = 2, sticky= 'e')
+        # -
+        self.pw_phone1_label = tk.Label(self,text="-", bg='snow2')
+        self.pw_phone1_label.grid(row = 8, column = 3)
+        # 연락처 TEXT2
+        self.pw_phone2_textfield = tk.Entry(self, bg = 'linen', width = 5)
+        self.pw_phone2_textfield.grid(row = 8, column = 4,sticky = 'ew')
+        # -
+        self.pw_phone2_label = tk.Label(self,text="-", bg='snow2')
+        self.pw_phone2_label.grid(row = 8, column = 5)
+        # 연락처 TEXT3
+        self.pw_phone3_textfield = tk.Entry(self, bg = 'linen', width = 6)
+        self.pw_phone3_textfield.grid(row = 8, column = 6, sticky= 'w')
+        # 엔터키 이벤트 설정
+        self.pw_phone3_textfield.bind('<Return>', self.pw_enter)
+
+        # 찾기 버튼
+        self.pw_search_btn = tk.Button(self,text="찾기", command = self.pw_search, borderwidth=0, bg='salmon2',activebackground="salmon3")
+        self.pw_search_btn.grid(row=9,column=2, sticky = 'ew', columnspan=4)
+
+        # 종료 버튼
+        self.quit_btn = tk.Button(self,text="종료", command = self.quit, borderwidth=0, bg='slategray1',activebackground="blue")
+        self.quit_btn.grid(row=10,column=0, sticky = 'nesw', columnspan=8)
+
+        # 각 컴포넌트 정렬
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=2)
+        self.grid_rowconfigure(2, weight=2)
+        self.grid_rowconfigure(3, weight=2)
+        self.grid_rowconfigure(4, weight=2)
+        self.grid_rowconfigure(5, weight=2)
+        self.grid_rowconfigure(6, weight=2)
+        self.grid_rowconfigure(7, weight=2)
+        self.grid_rowconfigure(8, weight=2)
+        self.grid_rowconfigure(9, weight=2)
+        self.grid_rowconfigure(10, weight=2)
+        self.grid_columnconfigure(0, weight=20)
+        self.grid_columnconfigure(1, weight=10)
+        self.grid_columnconfigure(2, weight=10)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
+        self.grid_columnconfigure(5, weight=1)
+        self.grid_columnconfigure(6, weight=10)
+        self.grid_columnconfigure(7, weight=20)
+
+        self.mainloop()
+
+    def id_search_validation(self) :
+        if self.id_name_textfield.get() :
+            if self.id_phone1_textfield.get() :
+                if self.id_phone2_textfield.get() :
+                    if self.id_phone3_textfield.get() :
+                        return True
+        else :
+            tk.messagebox.showerror('오류','아이디 찾기 정보 오류')
+            return False
+
+    def pw_search_validation(self) :
+        if self.pw_id_textfield.get() :
+            if self.pw_name_textfield.get() :
+                if self.pw_id_textfield.get() :
+                    if self.pw_phone1_textfield.get() :
+                        if self.pw_phone2_textfield.get() :
+                            if self.pw_phone3_textfield.get() :
+                                return True
+        else :
+            tk.messagebox.showerror('오류','비밀번호 찾기 정보 오류')
+            return False
+    def id_enter(self,event) :
+        if self.id_search_validation() :
+            self.id_search() 
+
+    def pw_enter(self,event) :
+        if self.pw_search_validation() :
+            self.pw_search()
+            return  
+
+    def id_search(self) :
+        if self.id_search_validation() :
+            name = self.id_name_textfield.get()
+            phone = self.id_phone1_textfield.get() + self.id_phone2_textfield.get() + self.id_phone3_textfield.get() 
+            ####  DB  ######
+            print('<아이디 찾기 정보>')
+            print('name : ' + name)
+            print('phone : ' + phone)
+            ###############
+            return
+
+    def pw_search(self) :
+        if self.pw_search_validation() :
+            id = self.pw_id_textfield.get()
+            name = self.pw_name_textfield.get()
+            phone = self.pw_phone1_textfield.get() + self.pw_phone2_textfield.get() + self.pw_phone3_textfield.get() 
+            #####  DB  ######
+            print('<비밀번호 찾기 정보>')
+            print('id : ' + id)
+            print('name : ' + name)
+            print('phone : ' + phone)
+            #################
+            return
+
+    def quit(self) :
+        self.destroy()
+        return
+
+
+if __name__ == "__main__" :
+    forget = Forget()
---
+++ minecraft_register.py
@@ -0,0 +1,230 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import Canvas, N, E, W, S
+from tkinter import messagebox
+from tkinter.constants import ANCHOR, CENTER
+from PIL import ImageTk, Image
+import time
+class Register(tk.Toplevel) :
+    def __init__(self):
+        super().__init__()
+        # tkinter 창 가로/세로 설정
+        self.width = int(self.winfo_screenwidth() / 5)
+        self.height = int(self.winfo_screenheight() / 3)
+
+        # 배경 투명 설정
+        self.wm_attributes('-transparentcolor',self['bg'])
+        
+        # 로그인 창 이미지 설정
+        self.image = ImageTk.PhotoImage(Image.open('assets/register.gif').resize((self.width,self.height), Image.ANTIALIAS))
+        
+        # 해상도 및 위치 설정
+        self.geometry('{}x{}+{}+{}'.format(self.width, self.height, int((self.winfo_screenwidth()-self.width)/2),int((self.winfo_screenheight()-self.height)/2)))
+        
+        # tk 설정
+        self.resizable(0,0)
+        self.title("회원가입")
+
+        # 배경 설정
+        self.bg_label = tk.Label(self,image=self.image) 
+        self.bg_label.place(x=0,y=0, relwidth = 1, relheight = 1)
+
+        self.id = ''
+        # ID
+        self.id_label = tk.Label(self,text="ID", bg='snow2')
+        self.id_label.grid(row = 1, column = 1)
+        # ID TEXT
+        self.id_textfield = tk.Entry(self, bg = 'linen')
+        self.id_textfield.grid(row = 1, column = 2,columnspan=5)
+
+        # PW
+        self.pw_label = tk.Label(self,text="PW", bg='snow2')
+        self.pw_label.grid(row = 2, column = 1)
+        # PW TEXT
+        self.pw_textfield = tk.Entry(self, bg = 'linen',show="*")
+        self.pw_textfield.grid(row = 2, column = 2,columnspan=5)
+
+        # PW확인 유효성 검증 변수
+        self.pw_equal = False
+        # PW확인
+        self.pw_check_label = tk.Label(self,text="PW확인", bg='snow2')
+        self.pw_check_label.grid(row = 3, column = 1)
+        # PW확인 TEXT
+        self.pw_check_textfield = tk.Entry(self, bg = 'linen',show="*")
+        self.pw_check_textfield.grid(row = 3, column = 2,columnspan=5)
+        
+        # 이름
+        self.name_label = tk.Label(self,text="이름", bg='snow2')
+        self.name_label.grid(row = 4, column = 1)
+        # 이름 TEXT
+        self.name_textfield = tk.Entry(self, bg = 'linen')
+        self.name_textfield.grid(row = 4, column = 2,columnspan=5)
+
+        # 연락처
+        self.phone_label = tk.Label(self,text="연락처", bg='snow2')
+        self.phone_label.grid(row = 5, column = 1)
+        # 연락처 TEXT1
+        self.phone1_textfield = tk.Entry(self, bg = 'linen', width = 4)
+        self.phone1_textfield.grid(row = 5, column = 2, sticky= 'e')
+        # -
+        self.phone1_label = tk.Label(self,text="-", bg='snow2')
+        self.phone1_label.grid(row = 5, column = 3)
+        # 연락처 TEXT2
+        self.phone2_textfield = tk.Entry(self, bg = 'linen', width = 5)
+        self.phone2_textfield.grid(row = 5, column = 4,sticky = 'ew')
+        # -
+        self.phone2_label = tk.Label(self,text="-", bg='snow2')
+        self.phone2_label.grid(row = 5, column = 5)
+        # 연락처 TEXT3
+        self.phone3_textfield = tk.Entry(self, bg = 'linen', width = 6)
+        self.phone3_textfield.grid(row = 5, column = 6, sticky= 'w')
+
+        # 주소
+        self.address_label = tk.Label(self,text="주소", bg='snow2')
+        self.address_label.grid(row = 6, column = 1)
+        # 주소 TEXT
+        self.address_textfield = tk.Entry(self, bg = 'linen', width = 30)
+        self.address_textfield.grid(row = 6, column = 2,columnspan=5)
+
+        # 이메일 콤보박스 목록
+        self.emails = ['naver.com','daum.net','gmail.com','nate.com','kakao.com','기타']
+        # E-MAIL
+        self.email_label = tk.Label(self,text="E-MAIL(선택)", bg='snow2')
+        self.email_label.grid(row = 7, column = 1)
+        # E-MAIL TEXT
+        self.email_textfield = tk.Entry(self, bg = 'linen',width = 10)
+        self.email_textfield.grid(row = 7, column = 2, columnspan = 2, sticky = 'e')
+        # @
+        self.email1_label = tk.Label(self,text="@", bg='snow2')
+        self.email1_label.grid(row = 7, column = 4)
+        # combo박스
+        self.combobox = ttk.Combobox(self,width = 10,values=self.emails)
+        self.combobox.grid(row=7, column=5, columnspan = 2, sticky = 'w')
+        self.combobox.bind("<<ComboboxSelected>>",self.gita)
+
+        # 가입 신청 버튼
+        self.register_btn = tk.Button(self,text="가입 신청", command = self.register, borderwidth=0, bg='slategray1',activebackground="blue")
+        self.register_btn.grid(row=9,column=0, sticky = 'nesw', columnspan=8)
+
+        # 주소와 email필드에 엔터키 바인딩
+        self.address_textfield.bind('<Return>', self.enter)
+        self.email_textfield.bind('<Return>', self.enter)
+
+        # 각 컴포넌트 정렬
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=2)
+        self.grid_rowconfigure(2, weight=2)
+        self.grid_rowconfigure(3, weight=2)
+        self.grid_rowconfigure(4, weight=2)
+        self.grid_rowconfigure(5, weight=2)
+        self.grid_rowconfigure(6, weight=2)
+        self.grid_rowconfigure(7, weight=2)
+        self.grid_rowconfigure(8, weight=1)
+        self.grid_rowconfigure(9, weight=2)
+        self.grid_columnconfigure(0, weight=20)
+        self.grid_columnconfigure(1, weight=10)
+        self.grid_columnconfigure(2, weight=10)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
+        self.grid_columnconfigure(5, weight=1)
+        self.grid_columnconfigure(6, weight=10)
+        self.grid_columnconfigure(7, weight=5)
+
+        self.login_success = False # 로그인 성공 여부
+        self.closed = True # 창 닫기 버튼
+
+        # id필드에 포커스
+        self.id_textfield.focus_set()
+        
+        # 0.1초마다 update 함수
+        self.update()
+
+        self.mainloop()
+    
+    # 콤보박스 '기타' 클릭 시 빈칸 설정
+    def gita(self,event) :
+        if self.combobox.get() == '기타' :
+            self.combobox.set('')
+            
+    # 엔터키 입력 시 가입 신청 진행
+    def enter(self, event) :
+        self.register()
+
+    # pw 및 필수 칸 유효성 검증
+    def validation(self) :
+        # ID 필드
+        if not self.id_textfield.get() :
+            tk.messagebox.showerror('오류','아이디 입력하세요.')
+            return False     
+        # PW 및 PW확인
+        if self.pw_textfield.get() :
+            if not self.pw_equal :
+                tk.messagebox.showerror('오류','비밀번호 확인이 일치하지 않습니다.')
+                return False
+        else :
+            tk.messagebox.showerror('오류','비밀번호를 입력하세요.')
+            return False
+        # name 필드
+        if not self.name_textfield.get() :
+            tk.messagebox.showerror('오류','이름을 입력하세요.')
+            return False
+        # 연락처
+        if not self.phone1_textfield.get() or not self.phone3_textfield.get() or not self.phone3_textfield.get() :
+            tk.messagebox.showerror('오류','연락처를 입력하세요.')
+            return False
+        # 주소
+        if not self.address_textfield.get() :
+            tk.messagebox.showerror('오류','주소를 입력하세요.')
+            return False
+
+        return True
+
+    def register(self) :
+        # 유효성 검증 후
+        if not self.validation() :
+            self.focus_set()
+            return 
+        
+        # 변수에 데이터 담기
+        id = self.id_textfield.get()
+        pw = self.pw_textfield.get()
+        name = self.name_textfield.get()
+        phone = self.phone1_textfield.get() + self.phone2_textfield.get() + self.phone3_textfield.get()
+        address = self.address_textfield.get()
+        email = self.email_textfield.get() + "@" + self.combobox.get()
+        #########  DB  ###############
+        print("id :" + id)
+        print("pw :" + pw)
+        print("name :" + name)
+        print("phone :" + phone)
+        print("address :" + address)
+        print("email :" + email)
+        ##########  DB  ##############
+        self.destroy()
+        tk.messagebox.showinfo('성공!','정상적으로 회원가입이 되었습니다.')
+        
+
+
+    def update(self) :
+        # PW & PW확인 체크
+        pw = self.pw_textfield.get()
+        pw_check = self.pw_check_textfield.get()
+        if pw != '' :
+            if pw_check != '' : 
+                if pw != pw_check :
+                    self.pw_equal = False
+                    self.pw_check_textfield.configure(bg='orange red')
+                else  :
+                    self.pw_equal = True
+                    self.pw_check_textfield.configure(bg='limegreen')
+            else : 
+                self.pw_check_textfield.configure(bg='linen')
+        else :       
+            self.pw_check_textfield.configure(bg='linen')
+        # update 주기 : 0.1초
+        self.after(100,self.update)
+    
+
+
+if __name__ == "__main__" :
+    register = Register()
