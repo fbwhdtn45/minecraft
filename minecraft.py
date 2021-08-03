7b91cd94f2f789aa5ec3dd017f8f2fe43e87b9cb
fbwhdtn45 <fbwhdtn5882@naver.com>
2021-08-03 18:04:06 +0900

    commit_0803

--- minecraft.py
+++ minecraft.py
@@ -13,45 +13,11 @@ from ursina.prefabs.clock import Clock
 from ursina.prefabs.background import Background
 import math
 from ursina.prefabs.hand import Hand
+from ursina.prefabs.dropitem import Dropitem
+import pyautogui
+from ursina.prefabs.tabpanel import Tabpanel
 
 class Minecraft(Entity) :
-    app = Ursina()
-
-    window.fps_counter.enabled = False
-
-    # 블록 배열
-    blocks = [
-        load_texture('assets/block/stone.png'), # 0
-        load_texture('assets/block/brick.png'), # 1
-        load_texture('assets/block/glass.png'), # 2
-        load_texture('assets/block/quartz.png'), # 3
-        load_texture('assets/block/gold.png'), # 4
-        load_texture('assets/block/diamond.png'), # 5
-        load_texture('assets/block/emerald.png'), # 6
-        load_texture('assets/block/amethyst.png'), # 7
-        load_texture('assets/block/honey.png'), # 8
-        load_texture('assets/block/slime.png'), # 9
-        load_texture('assets/block/redsand.png'), # 10
-        load_texture('assets/block/redstone.png'), # 11
-        load_texture('assets/block/bluestone.png'), # 12
-        load_texture('assets/concrete/white.png'), # 13
-        load_texture('assets/concrete/red.png'), # 14
-        load_texture('assets/concrete/purple.png'), # 15
-        load_texture('assets/concrete/pink.png'), # 16
-        load_texture('assets/concrete/orange.png'), # 17
-        load_texture('assets/concrete/lime.png'), # 18
-        load_texture('assets/concrete/lightblue.png'), # 19
-        load_texture('assets/concrete/green.png'), # 20
-        load_texture('assets/concrete/gray.png'), # 21
-        load_texture('assets/concrete/cyan.png'), # 22
-        load_texture('assets/concrete/blue.png'), # 23
-        load_texture('assets/concrete/black.png'), # 24
-        load_texture('assets/concrete/brown.png'), # 25
-        load_texture('assets/concrete/magenta.png'), # 26
-        load_texture('assets/concrete/lightgray.png'), # 27
-        load_texture('assets/concrete/yellow.png') # 28
-    ]
-
     def __init__(self):
         super().__init__()
 
@@ -71,7 +37,7 @@ class Minecraft(Entity) :
         self.heart = Button(icon='./assets/heart.png', scale = .05, color = color.clear, position = (-.4,-.365,-.1)) 
 
         # 탭 패널
-        self.tab_panel = Button(color=color.black33, scale = .2, position = (-.4 * window.aspect_ratio, .0))
+        self.tab_panel = Tabpanel(self.player)
 
         # 퀵 슬롯
         self.quickslot = Quickslot()
@@ -86,24 +52,48 @@ class Minecraft(Entity) :
         # 현재 시간
         self.clock = Clock()
         
+        # 죽었을 때 패널
+        self.die_panel = None
+
+        # 블록 배열
+        self.blocks = [
+            load_texture('assets/block/stone.png'), # 0
+            load_texture('assets/block/brick.png'), # 1
+            load_texture('assets/block/glass.png'), # 2
+            load_texture('assets/block/quartz.png'), # 3
+            load_texture('assets/block/gold.png'), # 4
+            load_texture('assets/block/diamond.png'), # 5
+            load_texture('assets/block/emerald.png'), # 6
+            load_texture('assets/block/amethyst.png'), # 7
+            load_texture('assets/block/honey.png'), # 8
+            load_texture('assets/block/slime.png'), # 9
+            load_texture('assets/block/redsand.png'), # 10
+            load_texture('assets/block/redstone.png'), # 11
+            load_texture('assets/block/bluestone.png'), # 12
+            load_texture('assets/concrete/white.png'), # 13
+            load_texture('assets/concrete/red.png'), # 14
+            load_texture('assets/concrete/purple.png'), # 15
+            load_texture('assets/concrete/pink.png'), # 16
+            load_texture('assets/concrete/orange.png'), # 17
+            load_texture('assets/concrete/lime.png'), # 18
+            load_texture('assets/concrete/lightblue.png'), # 19
+            load_texture('assets/concrete/green.png'), # 20
+            load_texture('assets/concrete/gray.png'), # 21
+            load_texture('assets/concrete/cyan.png'), # 22
+            load_texture('assets/concrete/blue.png'), # 23
+            load_texture('assets/concrete/black.png'), # 24
+            load_texture('assets/concrete/brown.png'), # 25
+            load_texture('assets/concrete/magenta.png'), # 26
+            load_texture('assets/concrete/lightgray.png'), # 27
+            load_texture('assets/concrete/yellow.png') # 28
+        ]
         # 초기 지형 생성 ---------------- 수정하기
         for z in range(-5,5) :
             for x in range(-5,5) :
-                self.voxel = Voxel(position=(x,0,z))
-
-        # 실행
-        Minecraft.app.run()
+                self.voxel = Voxel(position=(x,0,z),player = self.player)
     
     # 키보드 입력받기
     def input(self,key) :
-        # F1 : 자유모드 / 중력모드 스위칭
-        if key == 'f1' :
-            if self.player.gravity == 0 :
-                self.player.gravity = 1
-            else :
-                self.player.gravity = 0
-            return
-        
         # e 누르면 생성or삭제 // esc 누르면 삭제
         if key == 'e' :
             # 인벤토리 삭제
@@ -122,7 +112,7 @@ class Minecraft(Entity) :
                 self.player.on_disable()
                 # 인벤토리 첫 생성일 경우,
                 if self.is_first :
-                    for e in Minecraft.blocks :
+                    for e in self.blocks :
                         self.inventory.append(self.inventory.item_slot, e)
                     self.is_first = False
 
@@ -189,24 +179,13 @@ class Minecraft(Entity) :
                 # 1인칭 사용해제
                 self.player.on_disable()
                 # 죽었을 때 패널 생성
-                die_panel = Button(scale = (3,1), color = color.black66, position = (0,0,-.99), text = 'You Died!!',disabled = True)
+                self.die_panel = Button(parent = camera.ui,scale = (3,1), color = color.black66, position = (0,0,-.99), text = 'You Died!!',disabled = True)
                 # restart 버튼
-                restart_btn = Button(parent = die_panel,scale = (.1,.05),color = color.black,text_color = color.white,text = 'Restart : [ R ]',position = (-.1,-.15,-.1), _on_click = self.restart)
+                restart_btn = Button(parent = self.die_panel,scale = (.1,.05),color = color.black,text_color = color.white,text = 'Restart : [ R ]',position = (-.1,-.15,-.1), _on_click = self.restart)
                 # quit 버튼
-                quit_btn = Button(parent = die_panel,scale = (.1,.05),color = color.black,text_color = color.white,text = 'Quit : [ Q ]',position = (.1,-.15,-.1), _on_click = self.quit)
+                quit_btn = Button(parent = self.die_panel,scale = (.1,.05),color = color.black,text_color = color.white,text = 'Quit : [ Q ]',position = (.1,-.15,-.1), _on_click = self.quit)
             else :
                 return
-
-        # 탭 누르는 동안 tab_panel 보여주기
-        if held_keys['tab'] : 
-            # 탭 패널 생성
-            self.tab_panel.text = "My Position" + "\n"
-            self.tab_panel.text += "X : " + str(round(self.player.position.x,1)) + "\n"
-            self.tab_panel.text += "Y : " + str(round(self.player.position.y,1)) + "\n"
-            self.tab_panel.text += "Z : " + str(round(self.player.position.z,1))
-            self.tab_panel.visible = True
-        else : 
-            self.tab_panel.visible = False
 
         # 인벤토리가 있으면, 인벤토리 퀵 슬롯이랑 퀵 슬롯이랑 동기화시키기
         if self.inventory : 
@@ -215,7 +194,8 @@ class Minecraft(Entity) :
                 self.quickslot.append(e)
 
     def restart(self) :
-        print('다시하기')
+        destroy(self)
+        pyautogui.press('r')
         return
 
     # 게임 종료
@@ -227,4 +207,15 @@ if __name__ == "__main__" :
     # if login.success :
     #     minecraft = Minecraft()
 
+    app = Ursina()
+
+    window.fps_counter.enabled = False
+
     minecraft = Minecraft()
+
+    def input(key) :
+        global minecraft
+        if key == 'r' :
+            minecraft = Minecraft()
+
+    app.run()
